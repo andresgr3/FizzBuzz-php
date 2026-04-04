a9fbb3abb1e4850d88b357ae91ee159880129c52
Andres Granda <andresgranda@MacBook-Pro-de-Andres.local>
2026-04-04 20:04:55 +0200

    [GREEN]: givenFiveReturnBuzz

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -7,6 +7,9 @@ class FizzBuzz
         if ($number === 3){
             return "Fizz";
         }
+        if($number === 5){
+            return "Buzz";
+        }
         return (string) $number;
     }
 }
