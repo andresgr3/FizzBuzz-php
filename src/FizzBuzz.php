b0bd0da4c1b2174f8fa0446fe8bef753dcd4ea95
Andres Granda <andresgranda@MacBook-Pro-de-Andres.local>
2026-05-26 11:14:33 +0200

    [GREEN] - Return Buzz when number is five

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -14,6 +14,9 @@ class FizzBuzz
         if($number === 3){
             return "Fizz";
         }
+        if ($number === 5){
+            return "Buzz";
+        }
         return (string)$number;
     }
 
