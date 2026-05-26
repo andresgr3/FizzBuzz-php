6f05312b350187f2a4c769db7b86c80cb360c62b
Andres Granda <andresgranda@MacBook-Pro-de-Andres.local>
2026-05-26 15:51:49 +0200

    [GREEN] - Return FizzBuzz when number is multiple of five and three

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -11,6 +11,9 @@ class FizzBuzz
      */
     
     function getWordForNumber(int $number) : string {
+        if($number % 3 === 0 && $number % 5 === 0){
+            return "FizzBuzz";
+        }
         if($number % 3 === 0){
             return "Fizz";
         }
