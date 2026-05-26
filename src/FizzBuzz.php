a9a5355761e603c7e7d8a66a9740eeb0c9a668af
Andres Granda <andresgranda@MacBook-Pro-de-Andres.local>
2026-05-26 10:19:01 +0200

    Kata FizzBuzz desde cero

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -1,19 +1,16 @@
 <?php
+
 namespace Deg540\CleanCodeKata9;
 
 class FizzBuzz
 {
-    function execute(int $number): string {
-        $result = "";
-        if($number % 3 === 0){
-            $result .= "Fizz";
-        }
-        
-        if($number % 5 === 0){
-            $result .= "Buzz";
-        }
-
-        return empty($result) ? (string) $number : $result;
-
+    /**
+     * @param $value
+     *
+     * @return bool
+     */
+    
+    function integerChecker($value): bool {
+        return is_integer($value);
     }
 }
